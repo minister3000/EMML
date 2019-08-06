af3d49e61d78d7152b40642e38bb6b73dab395a3
minister3000 <tobias.gummersbach@gmx.de>
2019-08-06 01:20:42 -0400

    after compile

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,97 @@
 
 using namespace Rcpp;
 
+// LogLikeliHood_FULL
+Rcpp::List LogLikeliHood_FULL(Rcpp::NumericVector param, Rcpp::NumericMatrix datax, int n_explanatory, int n_transition, double prob_initial_state_0, int printLogLike);
+RcppExport SEXP _EMML_LogLikeliHood_FULL(SEXP paramSEXP, SEXP dataxSEXP, SEXP n_explanatorySEXP, SEXP n_transitionSEXP, SEXP prob_initial_state_0SEXP, SEXP printLogLikeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type param(paramSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type datax(dataxSEXP);
+    Rcpp::traits::input_parameter< int >::type n_explanatory(n_explanatorySEXP);
+    Rcpp::traits::input_parameter< int >::type n_transition(n_transitionSEXP);
+    Rcpp::traits::input_parameter< double >::type prob_initial_state_0(prob_initial_state_0SEXP);
+    Rcpp::traits::input_parameter< int >::type printLogLike(printLogLikeSEXP);
+    rcpp_result_gen = Rcpp::wrap(LogLikeliHood_FULL(param, datax, n_explanatory, n_transition, prob_initial_state_0, printLogLike));
+    return rcpp_result_gen;
+END_RCPP
+}
+// LogLikeliHood
+double LogLikeliHood(Rcpp::NumericVector param, Rcpp::NumericMatrix datax, int n_explanatory, int n_transition, double prob_initial_state_0, int printLogLike);
+RcppExport SEXP _EMML_LogLikeliHood(SEXP paramSEXP, SEXP dataxSEXP, SEXP n_explanatorySEXP, SEXP n_transitionSEXP, SEXP prob_initial_state_0SEXP, SEXP printLogLikeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type param(paramSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type datax(dataxSEXP);
+    Rcpp::traits::input_parameter< int >::type n_explanatory(n_explanatorySEXP);
+    Rcpp::traits::input_parameter< int >::type n_transition(n_transitionSEXP);
+    Rcpp::traits::input_parameter< double >::type prob_initial_state_0(prob_initial_state_0SEXP);
+    Rcpp::traits::input_parameter< int >::type printLogLike(printLogLikeSEXP);
+    rcpp_result_gen = Rcpp::wrap(LogLikeliHood(param, datax, n_explanatory, n_transition, prob_initial_state_0, printLogLike));
+    return rcpp_result_gen;
+END_RCPP
+}
+// LogLikeliHood_byObs
+Rcpp::NumericVector LogLikeliHood_byObs(Rcpp::NumericVector param, Rcpp::NumericMatrix datax, int n_explanatory, int n_transition, double prob_initial_state_0, int printLogLike);
+RcppExport SEXP _EMML_LogLikeliHood_byObs(SEXP paramSEXP, SEXP dataxSEXP, SEXP n_explanatorySEXP, SEXP n_transitionSEXP, SEXP prob_initial_state_0SEXP, SEXP printLogLikeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type param(paramSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type datax(dataxSEXP);
+    Rcpp::traits::input_parameter< int >::type n_explanatory(n_explanatorySEXP);
+    Rcpp::traits::input_parameter< int >::type n_transition(n_transitionSEXP);
+    Rcpp::traits::input_parameter< double >::type prob_initial_state_0(prob_initial_state_0SEXP);
+    Rcpp::traits::input_parameter< int >::type printLogLike(printLogLikeSEXP);
+    rcpp_result_gen = Rcpp::wrap(LogLikeliHood_byObs(param, datax, n_explanatory, n_transition, prob_initial_state_0, printLogLike));
+    return rcpp_result_gen;
+END_RCPP
+}
+// LogLikeliHood_min
+double LogLikeliHood_min(Rcpp::NumericVector param, Rcpp::NumericMatrix datax, int n_explanatory, int n_transition, double prob_initial_state_0, int printLogLike);
+RcppExport SEXP _EMML_LogLikeliHood_min(SEXP paramSEXP, SEXP dataxSEXP, SEXP n_explanatorySEXP, SEXP n_transitionSEXP, SEXP prob_initial_state_0SEXP, SEXP printLogLikeSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type param(paramSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type datax(dataxSEXP);
+    Rcpp::traits::input_parameter< int >::type n_explanatory(n_explanatorySEXP);
+    Rcpp::traits::input_parameter< int >::type n_transition(n_transitionSEXP);
+    Rcpp::traits::input_parameter< double >::type prob_initial_state_0(prob_initial_state_0SEXP);
+    Rcpp::traits::input_parameter< int >::type printLogLike(printLogLikeSEXP);
+    rcpp_result_gen = Rcpp::wrap(LogLikeliHood_min(param, datax, n_explanatory, n_transition, prob_initial_state_0, printLogLike));
+    return rcpp_result_gen;
+END_RCPP
+}
+// LogLikeliHood_RunTimeCheck
+double LogLikeliHood_RunTimeCheck(Rcpp::NumericVector param, Rcpp::NumericMatrix datax, int n_explanatory, int n_transition, double prob_initial_state_0, int printLogLike, int n_it);
+RcppExport SEXP _EMML_LogLikeliHood_RunTimeCheck(SEXP paramSEXP, SEXP dataxSEXP, SEXP n_explanatorySEXP, SEXP n_transitionSEXP, SEXP prob_initial_state_0SEXP, SEXP printLogLikeSEXP, SEXP n_itSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type param(paramSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericMatrix >::type datax(dataxSEXP);
+    Rcpp::traits::input_parameter< int >::type n_explanatory(n_explanatorySEXP);
+    Rcpp::traits::input_parameter< int >::type n_transition(n_transitionSEXP);
+    Rcpp::traits::input_parameter< double >::type prob_initial_state_0(prob_initial_state_0SEXP);
+    Rcpp::traits::input_parameter< int >::type printLogLike(printLogLikeSEXP);
+    Rcpp::traits::input_parameter< int >::type n_it(n_itSEXP);
+    rcpp_result_gen = Rcpp::wrap(LogLikeliHood_RunTimeCheck(param, datax, n_explanatory, n_transition, prob_initial_state_0, printLogLike, n_it));
+    return rcpp_result_gen;
+END_RCPP
+}
+// ReturnTiny
+double ReturnTiny();
+RcppExport SEXP _EMML_ReturnTiny() {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    rcpp_result_gen = Rcpp::wrap(ReturnTiny());
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_hello
 List rcpp_hello();
 RcppExport SEXP _EMML_rcpp_hello() {
@@ -17,6 +108,12 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_EMML_LogLikeliHood_FULL", (DL_FUNC) &_EMML_LogLikeliHood_FULL, 6},
+    {"_EMML_LogLikeliHood", (DL_FUNC) &_EMML_LogLikeliHood, 6},
+    {"_EMML_LogLikeliHood_byObs", (DL_FUNC) &_EMML_LogLikeliHood_byObs, 6},
+    {"_EMML_LogLikeliHood_min", (DL_FUNC) &_EMML_LogLikeliHood_min, 6},
+    {"_EMML_LogLikeliHood_RunTimeCheck", (DL_FUNC) &_EMML_LogLikeliHood_RunTimeCheck, 7},
+    {"_EMML_ReturnTiny", (DL_FUNC) &_EMML_ReturnTiny, 0},
     {"_EMML_rcpp_hello", (DL_FUNC) &_EMML_rcpp_hello, 0},
     {NULL, NULL, 0}
 };
